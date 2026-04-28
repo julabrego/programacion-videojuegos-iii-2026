101c73fbed945b7dff27d3ed439d10709c37540d
julabrego <abrego.jul@gmail.com>
2026-04-27 22:52:30 -0300

    fix: improves player movement

--- actividad-02/player/player.gd
+++ actividad-02/player/player.gd
@@ -1,20 +1,15 @@
-
 extends CharacterBody2D
 
-const MOTION_SPEED = 160 # Pixels/second
+@export var max_speed = 200.0
+@export var acceleration = 600.0
+@export var deceleration = 500.0
 
 func _physics_process(delta):
-	var motion = Vector2()
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_bottom")
+	if direction != Vector2.ZERO:
+		var target_velocity = direction * max_speed
+		velocity = velocity.move_toward(target_velocity, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
 	
-	if (Input.is_action_pressed("move_up")):
-		motion = Vector2(0, -1)
-	elif (Input.is_action_pressed("move_bottom")):
-		motion = Vector2(0, 1)
-	elif (Input.is_action_pressed("move_left")):
-		motion = Vector2(-1, 0)
-	elif (Input.is_action_pressed("move_right")):
-		motion = Vector2(1, 0)
-	
-	motion = motion.normalized()*MOTION_SPEED*delta
-	move_and_collide(motion)
-
+	move_and_slide()
