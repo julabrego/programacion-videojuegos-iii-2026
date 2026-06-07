17f04dde2ca0a00f372d7ed511f7124ea3d83085
Julabrego <abrego.jul@gmail.com>
2026-06-06 23:47:00 -0300

    refactor: moves FrogController to FrogIdle node

--- actividad-05/scripts/FrogController.gd
+++ actividad-05/scripts/FrogController.gd
@@ -1,14 +1,13 @@
-extends Node2D
+extends Sprite2D
 class_name FrogController
 
 func _ready():
 	pass
-
+	
 func _on_game_panel_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and event.get_button_index() == MouseButton.MOUSE_BUTTON_LEFT:
-		var local_position = event.position
-		var global_position = get_global_transform().affine_inverse() * event.global_position
-		%FrogIdle.position = global_position
+		var new_position = get_parent().to_local(event.global_position)
+		position = new_position
 
-		GameState.set_frog_position(local_position)
+		GameState.set_frog_position(new_position)
 		GameState.add_score(1)
