6605b65b45a42fe1bb1626ac187bffafe7150077
Julabrego <abrego.jul@gmail.com>
2026-06-07 16:50:07 -0300

    feat: Shows data text at scrollable panel

--- actividad-05/scripts/PersistenceManager.gd
+++ actividad-05/scripts/PersistenceManager.gd
@@ -18,7 +18,7 @@ func _ready():
 	var save_dict = json.get_data()
 	if save_dict is Dictionary and save_dict.has("game"):
 		var has_data = true
-		GameState.persisted_data_changed.emit(has_data)
+		GameState.persisted_data_changed.emit(has_data, JSON.stringify(save_dict, "  "))
 	else:
 		GameState.persisted_data_changed.emit(false)
 
@@ -40,13 +40,18 @@ func save_game_v0_1() -> void:
 	}
 	
 	file.store_line(JSON.stringify(save_dict))
-	GameState.persisted_data_changed.emit(true)
+	GameState.persisted_data_changed.emit(true,  JSON.stringify(save_dict, "  "))
 
 func load_game_v0_1():
 	var file := FileAccess.open(SAVE_PATH, FileAccess.READ)
 	var json := JSON.new()
 	json.parse(file.get_line())
 	var save_dict := json.get_data() as Dictionary
+	
+	var version = str_to_var(save_dict.version)
+	if version > 0.1:
+		print("version error")
+		return
 
 	var frog_position = str_to_var(save_dict.game.frog_position)
 	var score = str_to_var(save_dict.game.score)
@@ -55,6 +60,47 @@ func load_game_v0_1():
 	GameState.set_frog_position(frog_position)
 	GameState.set_score(score)
 	Settings.set_bgm_enabled(bgm_enabled)
+
+func save_game_v0_3() -> void:
+	var file := FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+
+	var frog_position = GameState.frog_position
+	var score = GameState.score
+	var bgm_enabled = Settings.bgm_enabled
+	var sfx_enabled = Settings.sfx_enabled
+	
+	var save_dict = {
+		version = "0.3",
+		game = {
+			frog_position = var_to_str(frog_position),
+			score = var_to_str(score)
+		},
+		configuration = {
+			bgm_enabled = bgm_enabled,
+			sfx_enabled = sfx_enabled
+		}
+	}
+	
+	file.store_line(JSON.stringify(save_dict))
+	GameState.persisted_data_changed.emit(true, JSON.stringify(save_dict, "  "))
+
+func load_game_v0_3():
+	var file := FileAccess.open(SAVE_PATH, FileAccess.READ)
+	var json := JSON.new()
+	json.parse(file.get_line())
+	var save_dict := json.get_data() as Dictionary
+
+	var frog_position = str_to_var(save_dict.game.frog_position)
+	var score = str_to_var(save_dict.game.score)
+	var bgm_enabled = save_dict.configuration.bgm_enabled
+	var sfx_enabled = save_dict.configuration.sfx_enabled if save_dict.configuration.has("sfx_enabled") else Settings.sfx_enabled
+	
+	print("sfx_enabled %s" % sfx_enabled)
+	
+	GameState.set_frog_position(frog_position)
+	GameState.set_score(score)
+	Settings.set_bgm_enabled(bgm_enabled)
+	Settings.set_sfx_enabled(sfx_enabled)
 	
 func clear_data() -> void:
 	var file := FileAccess.open(SAVE_PATH, FileAccess.WRITE)
--- actividad-05/scripts/UIManager.gd
+++ actividad-05/scripts/UIManager.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 	GameState.score_changed.connect(update_score_text)
 	GameState.frog_position_changed.connect(update_frog_position_text)
 	Settings.bgm_enabled_changed.connect(update_bgm_enabled_switch)
+	Settings.sfx_enabled_changed.connect(update_sfx_enabled_switch)
 	GameState.persisted_data_changed.connect(update_load_persisted_data_status)
 	
 	%SFXButton.button_pressed = Settings.sfx_enabled
@@ -17,12 +18,16 @@ func update_score_text(score: int):
 func update_frog_position_text(new_position: Vector2):
 	%Position.set_text("X: %0.2f, Y: %0.2f" % [new_position.x, new_position.y])
 
-func update_load_persisted_data_status(is_data_available: bool) -> void:
+func update_load_persisted_data_status(is_data_available: bool, data: String = "") -> void:
+	%SaveText.text = data
 	%Load01.disabled = not is_data_available
 	%Load03.disabled = not is_data_available
 	
 func update_bgm_enabled_switch(enabled: bool) -> void:
 	%BGMButton.button_pressed = enabled
+
+func update_sfx_enabled_switch(enabled: bool) -> void:
+	%SFXButton.button_pressed = enabled
 
 func _on_bgm_button_toggled(toggled_on: bool) -> void:
 	Settings.set_bgm_enabled(toggled_on)
